16bff8231830607d72a31f8089aed46605838697
Kushagra <kushagrasaxena03@gmail.com>
2020-11-03 19:44:40 +0530

    API RESPONSE PARAMETER SET

--- app/src/main/java/kushagra/demo/sliceit/Models/TwiterApiResponse.java
+++ app/src/main/java/kushagra/demo/sliceit/Models/TwiterApiResponse.java
@@ -1,4 +1,29 @@
 package kushagra.demo.sliceit.Models;
 
+
+import com.google.gson.annotations.Expose;
+        import com.google.gson.annotations.SerializedName;
+
+        import java.util.List;
+
 public class TwiterApiResponse {
+    @SerializedName("success")
+    @Expose
+    private String success;
+
+    @SerializedName("data")
+    @Expose
+    List<TweetDetail> data = null;
+
+
+
+    public String getSuccess() {
+        return success;
+    }
+
+    public List<TweetDetail> getData() {
+        return data;
+    }
+
+
 }

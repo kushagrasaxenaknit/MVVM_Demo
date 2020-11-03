08f0f3d8e3832f6a383c6ee1b8c501cba0b99db5
Kushagra <kushagrasaxena03@gmail.com>
2020-11-03 19:57:23 +0530

    API data RESPONSE PARAMETER SET

---
+++ app/src/main/java/kushagra/demo/sliceit/Services/ApiCalls.java
@@ -0,0 +1,56 @@
+package kushagra.demo.sliceit.Services;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class ApiCalls {
+
+    @SerializedName("name")
+    @Expose
+    private String name;
+
+    @SerializedName("handle")
+    @Expose
+    private String handle;
+
+    @SerializedName("favoriteCount")
+    @Expose
+    private int favoriteCount;
+
+    @SerializedName("retweetCount")
+    @Expose
+    private String retweetCount;
+
+    @SerializedName("profileImageUrl")
+    @Expose
+    private String profileImageUrl;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getHandle() {
+        return handle;
+    }
+
+    public int getFavoriteCount() {
+        return favoriteCount;
+    }
+
+    public String getRetweetCount() {
+        return retweetCount;
+    }
+
+    public String getProfileImageUrl() {
+        return profileImageUrl;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    @SerializedName("text")
+    @Expose
+    private String text;
+
+}
